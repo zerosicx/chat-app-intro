4a2930b6e23070fa6a05a82560e9d7299335df21
Hannah Carino <hcarino.official@gmail.com>
2022-12-08 23:37:05 +1300

    Implemented basic broadcasting functionality

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ server.listen(3000, () => {
 // Initialise socket io and get the chat message value
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+        io.emit('chat message', msg)
     });
 });
 
